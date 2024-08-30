dbf235f0e1b308a6dd0406813f6b74987b21ec43
Mateus Guedes <mxteusgued@gmail.com>
2024-08-29 23:04:58 -0300

    feature: create routes

---
+++ src/controller/routes/routes.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"github.com/MateusGuedess/crudzinho-go/src/controller"
+	"github.com/gin-gonic/gin"
+)
+
+func InitRoutes(router *gin.RouterGroup) {
+	router.GET("/user/:userId", controller.FindUserByID)
+	router.GET("/user/:userEmail", controller.FindUserByEmail)
+	router.POST("/user", controller.CreateUser)
+	router.PUT("/user/:userId", controller.UpdateUser)
+	router.DELETE("/user/:userId", controller.DeleteUser)
+}
