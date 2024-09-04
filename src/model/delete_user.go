5dee29bd69dd5359f8b8350d7e3ef10cc67b3061
Mateus Guedes <mxteusgued@gmail.com>
2024-09-03 21:12:21 -0300

    feature: create all domains path

---
+++ src/model/delete_user.go
@@ -0,0 +1,7 @@
+package model
+
+import "github.com/MateusGuedess/crudzinho-go/src/configuration/rest_err"
+
+func (userDomain *UserDomain) deleteUser() *rest_err.RestErr {
+	return nil
+}
---
+++ src/model/find_user.go
@@ -0,0 +1,7 @@
+package model
+
+import "github.com/MateusGuedess/crudzinho-go/src/configuration/rest_err"
+
+func (userDomain *UserDomain) FindUser() (*UserDomain, *rest_err.RestErr) {
+	return nil, nil
+}
